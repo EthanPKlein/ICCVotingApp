6d0716645f2a29224be406bd2ef8353eb2817cf3
EthanKlein <eklein@icct.com>
2016-07-27 19:26:54 -0400

    Added votes to JSON

--- src/components/Location.js
+++ src/components/Location.js
@@ -18,13 +18,16 @@ export default class Location extends React.Component {
     return (
       <div>
       <div className="panel panel-default">
-      <div className="panel-heading">{this.props.location.name}</div>
+        <div className="panel-heading">{this.props.location.name}</div>
         <div className="panel-body">
-        <img src={this.props.location.imageUrl} style={imageStyle} alt={this.props.location.name} />
-          <p>{this.props.location.description}</p>
-          <p><b>{this.props.location.address}</b></p>
+          <img src={this.props.location.imageUrl} style={imageStyle} alt={this.props.location.name} />
+            <p>{this.props.location.description}</p>
+            <p><b>{this.props.location.address}</b></p>
         </div>
-      </div>
+        <div className="panel-footer">
+            <span className="glyphicon-class">This venue has <b>{this.props.location.votes.length}</b> votes!</span>
+        </div>
+        </div>
       </div>);
 
   }
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -19,23 +19,24 @@ export default class Locations extends React.Component {
          "name": "Magic Mountain",
          "address": "8350 Lyra Dr, Columbus, OH 43240",
          "imageUrl": "http://www.bwcolumbushotel.com/images/CommanFile/EVTMagic_Mountain_PolarisBest_Western_Columbus_North_Hotel_-_Magic_Mountain_Polaris.jpg",
-         "description": "A lovely little place to go and play some mini golf with friends or zoom around with some go-karts!"
+         "description": "A lovely little place to go and play some mini golf with friends or zoom around with some go-karts!",
+         "votes": ["eklein@icct.com", "mrnobody@icct.com"]
        },
        {
          "id": 2,
          "name": "Buffalo Wild Wings",
          "address": "2151 N High St, Columbus, OH 43201",
          "imageUrl": "http://www.buffalowildwings.com/moreexcuses/images/slider-landing.jpg?1469404800022",
-         "description": "A nice little restaurant featuring wings of a wide variety of spices.  Has a good selection of beer on tap and the televisions always play the best sports games."
-
+         "description": "A nice little restaurant featuring wings of a wide variety of spices.  Has a good selection of beer on tap and the televisions always play the best sports games.",
+         "votes": ["ILikeChicken@icct.com"]
        },
        {
          "id": 3,
          "name": "Champions Golf Course",
          "address": "3900 Westerville Road, Columbus, OH 43224",
          "imageUrl": "http://imavex.vo.llnwd.net/o18/clients/columbusgolf/images/Golf_Course/course_overview_champions.jpg",
-         "description": "Big, grassy open course reasonable for ameteurs and veterans alike.  Comes with golf cart rentals and scuba gear for retrieving balls in the water traps!"
-
+         "description": "Big, grassy open course reasonable for ameteurs and veterans alike.  Comes with golf cart rentals and scuba gear for retrieving balls in the water traps!",
+         "votes": []
        }
      ]
    }
@@ -61,7 +62,6 @@ export default class Locations extends React.Component {
   }
 
   renderLocation(location, i) {
-    console.log(location);
     return(<Location key={i} location={location}/>);
   }
 
