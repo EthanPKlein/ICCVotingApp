a0d2310afe62837de800ffc756fbcae9e69c5ee5
EthanKlein <eklein@icct.com>
2016-07-27 19:16:43 -0400

    Stylized Location component

--- src/components/Location.js
+++ src/components/Location.js
@@ -7,10 +7,26 @@ export default class Location extends React.Component {
   }
 
   render() {
+
+    var imageStyle = {
+      border: '3px solid black',
+      maxHeight:'300px',
+      maxWidth:'300px',
+      margin: '10px'
+    };
+
     return (
       <div>
-
+      <div className="panel panel-default">
+      <div className="panel-heading">{this.props.location.name}</div>
+        <div className="panel-body">
+        <img src={this.props.location.imageUrl} style={imageStyle} alt={this.props.location.name} />
+          <p>{this.props.location.description}</p>
+          <p><b>{this.props.location.address}</b></p>
+        </div>
+      </div>
       </div>);
+
   }
 
 }
